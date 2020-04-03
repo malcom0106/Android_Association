d546d95d827b2aac83e4745d9f0f84a466677d8c
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-04-03 11:02:08 +0200

    MLise en place d'un appel async et sleep le thread

--- app/src/main/java/com/example/association/SplashActivity.java
+++ app/src/main/java/com/example/association/SplashActivity.java
@@ -2,6 +2,8 @@ package com.example.association;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,5 +12,36 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        try{
+            SleepTask sleepTask = new SleepTask();
+            sleepTask.execute();
+        }
+        catch (Exception ex){
+            String message = ex.getMessage();
+        }
+    }
+    private class SleepTask extends AsyncTask<String,String,String>{
+        @Override
+        protected String doInBackground(String... voids) {
+
+            //Simule un appel service web
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
