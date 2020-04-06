5f669a6128f17da25ea99ae840ff8777fb5f9038
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-04-06 09:21:59 +0200

    Mise a jour du code Constantes et CallSW

--- app/src/main/java/com/example/association/Utilities/CallServiceWeb.java
+++ app/src/main/java/com/example/association/Utilities/CallServiceWeb.java
@@ -1,10 +1,17 @@
 package com.example.association.Utilities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.association.Entities.ParametreOkHttp;
+import com.example.association.MainActivity;
+import com.example.association.SplashActivity;
 
 import java.util.ArrayList;
 
@@ -14,7 +21,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class CallServiceWeb {
-    public static String CallServiceWeb(String url,@Nullable ArrayList<ParametreOkHttp> parametres)
+    public static String CallServiceWeb(String url,@Nullable ArrayList<ParametreOkHttp> parameters)
     {
         String retourSW = "";
         //String url = "http://claudehenry.fr/serviceweb/LoginAdherent";
@@ -22,21 +29,24 @@ public class CallServiceWeb {
 
         OkHttpClient client = new OkHttpClient();
 
-        //Request request = new Request.Builder().url(url).get().build();
 
         HttpUrl.Builder httpBuider = HttpUrl.parse(url).newBuilder();
-        if(parametres != null){
-            if(parametres.size()>0){
+        if(parameters != null){
+            if(parameters.size()>0){
                 ParametreOkHttp parametreOkHttp = null;
-                for(int i = 0 ;  i < parametres.size(); i++){
-                    parametreOkHttp = parametres.get(i);
+                for(int i = 0 ;  i < parameters.size(); i++){
+                    parametreOkHttp = parameters.get(i);
                     httpBuider.addQueryParameter(parametreOkHttp.getKey(), parametreOkHttp.getValue());
                 }
             }
         }
 
 
-        Request request = new Request.Builder().url(httpBuider.build()).build();
+        Request request =
+                new Request.Builder()
+                .url(httpBuider.build())
+                .build();
+
         try{
             Response response = client.newCall(request).execute();
             if(response.isSuccessful()){
@@ -49,4 +59,42 @@ public class CallServiceWeb {
         Log.e("retourWS",retourSW);
         return retourSW;
     }
+
+    /* public static class AsyncTask extends android.os.AsyncTask<String,String,String> {
+
+        private AppCompatActivity _ActivityCurrent;
+        private String _Url;
+        private ArrayList<ParametreOkHttp> _Parametres;
+        private Context _context;
+
+        public AsyncTask(Context context, AppCompatActivity activity  , String url, @Nullable ArrayList<ParametreOkHttp> parametres) {
+            this._ActivityCurrent = activity;
+            this._context = context;
+            this._Url = url;
+            this._Parametres = parametres;
+        }
+
+        @Override
+        protected String doInBackground(String... voids) {
+            String message = "";
+            //Simule un appel service web
+            try {
+                //Thread.sleep(5000);
+                message = CallServiceWeb.CallServiceWeb(_Url, _Parametres);
+            } catch (Exception e) {
+                message = e.getMessage();
+            }
+            return message;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            Toast.makeText(_context, result, Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(_context, _ActivityCurrent.getClass());
+            intent.putExtra("Resultat", result);
+            _context.startActivity(intent);
+        }
+    } */
 }
